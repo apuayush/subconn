1958c7d06bfd9db0193e175b7ecccd87acc39aaa
nishnash54 <nishnash54@gmail.com>
2017-12-30 12:23:26 +0530

    Create new agent basic

---
+++ Admin_tools/create_agent.py
@@ -0,0 +1,40 @@
+from Tkinter import *
+from pymongo import MongoClient
+
+client = MongoClient()
+client = MongoClient('localhost', 27017)
+db = client['subconn']
+collection = db['agent_details']
+
+def agent():
+    uid = str(blank1.get())
+    aid = str(blank2.get())
+    pas = str(blank3.get())
+    post = {"uid": uid, "userid": aid, "password": pas, "Rice": 0, "Wheat": 0, "Sugar": 0, "Oil": 0}
+    db.agent_details.insert(post)
+    blank1.delete(0, 'end')
+    blank2.delete(0, 'end')
+    blank3.delete(0, 'end')
+
+
+main = Tk()
+main.resizable(0, 0)
+fnt = (None, 20)
+
+Label(main, text="Aadhaar number", font=fnt).grid(row=0)
+Label(main, text="Agent id", font=fnt).grid(row=1)
+Label(main, text="Password", font=fnt).grid(row=2)
+
+blank1 = Entry(main, font=fnt)
+blank1.grid(row=0, column=1)
+blank2 = Entry(main, font=fnt)
+blank2.grid(row=1, column=1)
+blank3 = Entry(main, font=fnt)
+blank3.grid(row=2, column=1)
+
+Button(main, text='Quit', bg='red', font=fnt, command=main.destroy).\
+    grid(row=3, column=0, sticky=W, pady=4)
+Button(main, text='Create',bg='green', font=fnt, command=agent).\
+    grid(row=3, column=1, sticky=W, pady=4)
+
+mainloop()
