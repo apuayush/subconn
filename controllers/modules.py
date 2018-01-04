31d7eea647fed12c3ec99fe75857b9ce91bb2391
apuayush <apuayush@gmail.com>
2018-01-05 00:21:30 +0530

    env

--- controllers/modules.py
+++ controllers/modules.py
@@ -22,10 +22,10 @@ import requests
 # hash libraries
 from hashlib import sha256
 #
-JWT_SECRET = env.JWT_SECRET
-JWT_ALGORITHM = env.JWT_ALGORITHM
-db = MotorClient(env.db)['subconn']
+# JWT_SECRET = env.JWT_SECRET
+# JWT_ALGORITHM = env.JWT_ALGORITHM
+# db = MotorClient(env.db)['subconn']
 #
-# JWT_SECRET = os.environ['JWT_SECRET']
-# JWT_ALGORITHM = os.environ['JWT_ALGORITHM']
-# db = MotorClient(os.environ['DB_LINK'])['subconn']
+JWT_SECRET = os.environ['JWT_SECRET']
+JWT_ALGORITHM = os.environ['JWT_ALGORITHM']
+db = MotorClient(os.environ['DB_LINK'])['subconn']
